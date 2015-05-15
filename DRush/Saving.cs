c95dd8e620b424a7bc07ba23aae4d09405b14c40
Alex Natarov <aleksnatarov@gmail.com>
2015-05-15 23:43:00 +0300

    Trying to impliment pattern Command

---
+++ DRush/Saving.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DRush
+{
+    class Saving
+    {
+        Saving()
+        {
+
+        }
+
+        public void Save () 
+        {
+            // Вызываем для сохранения.
+            // Принимаем сформированный список из всех элементов (TODO - аналог юниона или чего-нибудь такого)
+            // Дракон с координатами и характеристиками.
+            // Массив бегроанду
+
+        }
+
+        public void Load()
+        {
+
+        }
+
+        private void WriteSave()
+        {
+            // Пишем в xml все данные
+        }
+
+    }
+}
